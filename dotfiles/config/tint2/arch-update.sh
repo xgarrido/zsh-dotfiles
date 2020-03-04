b4177ce48b7fdb0385f29f9008cad207248dc907
Xavier Garrido <xavier.garrido@gmail.com>
2020-03-04 15:10:17 +0100

    add first executor (arch update)

---
+++ dotfiles/config/tint2/arch-update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Author: Piotr Miller
+# e-mail: nwg.piotr@gmail.com
+# Website: http://nwg.pl
+# Project: https://github.com/nwg-piotr/tint2-executors
+# License: GPL3
+
+# Credits: RaphaelRochet/arch-update
+# https://github.com/RaphaelRochet/arch-update
+# Icon by @edskeye
+
+upd=$(/bin/sh -c "/usr/bin/checkupdates")
+
+if [[ ! -z "$upd" ]]
+then
+    echo ~/.config/tint2/images/arch-icon-notify.svg
+    echo "($upd)" | wc -l
+    notify-send " Pending updates:" "<i>$upd</i>" --icon="archlinux" --expire-time=5000
+else
+    echo ~/.config/tint2/images/arch-icon.svg
+fi
